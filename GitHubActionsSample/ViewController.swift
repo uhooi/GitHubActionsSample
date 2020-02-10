d464ee3e9a2b8fe62b3b29c30886309900282dc6
uhooi <uhooi@server.fake>
2020-02-10 16:40:15 +0900

    Xcodeプロジェクトの作成

---
+++ GitHubActionsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubActionsSample
+//
+//  Created by uhooi on 2020/02/10.
+//  Copyright © 2020 THE Uhooi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
